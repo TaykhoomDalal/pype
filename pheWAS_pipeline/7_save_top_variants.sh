14656eabbaf9d814ea326af5fa9bf49b7ed0124d
TaykhoomDalal <tdalal@g.ucla.edu>
2022-07-10 21:01:27 -0400

    associated scripts for singular code files

---
+++ pheWAS_pipeline/7_save_top_variants.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -c 1                                                                                        # Request one core
+#SBATCH -t 0-00:30:00                                                                                # Runtime in D-HH:MM format
+#SBATCH -p short                                                                                    # Partition to run in
+#SBATCH --mem=64000M                                                                                # Memory total in MiB (for all cores)
+#SBATCH -o /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/save_top_variants_%j.out   # File to which STDOUT will be written, including job ID (%j)
+#SBATCH -e /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/save_top_variants_%j.err   # File to which STDERR will be written, including job ID (%j)
+
+python3 /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/pheWAS_pipeline/7_save_top_variants.py \
+        --url https://biobank.ndph.ox.ac.uk/showcase/label.cgi?id=$1 \
+        --out_dir /home/tad368/pheWAS
