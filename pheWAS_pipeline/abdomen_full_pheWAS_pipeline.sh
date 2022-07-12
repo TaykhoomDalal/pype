abe21560f8104a8f1a481f5106b473704e5a3f44
TaykhoomDalal <tdalal@g.ucla.edu>
2022-07-12 16:02:59 -0400

    add separate scripts for starting phewas

---
+++ pheWAS_pipeline/abdomen_full_pheWAS_pipeline.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -c 4                                                                                        # Request four cores
+#SBATCH -t 0-6:00:00                                                                               # Runtime in D-HH:MM format
+#SBATCH -p short                                                                                    # Partition to run in
+#SBATCH --mem=64000M                                                                                # Memory total in MiB (for all cores)
+#SBATCH -o /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/abdomen_pheWAS_pipeline%j.out   # File to which STDOUT will be written, including job ID (%j)
+#SBATCH -e /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/abdomen_pheWAS_pipeline%j.err   # File to which STDERR will be written, including job ID (%j)
+
+python3 ~/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/pheWAS_pipeline/full_pheWAS_pipeline.py \
+                                --phenotype /home/tad368/orig_phenos/ukb41230.tab \
+                                --bfiles ~/data_dir/bfiles \
+                                --covariates ~/data_dir/data_fields/cov_fields.txt \
+                                --url https://biobank.ndph.ox.ac.uk/showcase/label.cgi?id=$1 \
+                                --samples ~/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/in_pop_IDs/Abdomen_in_sample_pop_IDs.txt \
+                                --variants /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/variants_list/Abdomen/Abdomen_Top_SNPS.tsv \
+                                --samples ~/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/in_pop_IDs/Pancreas_in_sample_pop_IDs.txt \
+                                --variants /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/variants_list/Abdomen/Pancreas_Top_SNPS.tsv \
+                                --samples ~/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/in_pop_IDs/Liver_in_sample_pop_IDs.txt \
+                                --variants /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/variants_list/Abdomen/Liver_Top_SNPS.tsv \
+                                --threads 4 \
+                                --memory 64000 \
+                                --location ~/data_dir \
+                                --name ukbb_category_$1_abdomen
--- pheWAS_pipeline/full_batch_pheWAS_pipeline.sh
+++ pheWAS_pipeline/full_batch_pheWAS_pipeline.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-sbatch full_pheWAS_pipeline.sh 1006
-sbatch full_pheWAS_pipeline.sh 1003
-sbatch full_pheWAS_pipeline.sh 1019
-sbatch full_pheWAS_pipeline.sh 17518
-sbatch full_pheWAS_pipeline.sh 100081
-sbatch full_pheWAS_pipeline.sh 1307
+# if -abdomen is used, then run abd_full_pheWAS_pipeline.sh 
+if [ $1 == "-abdomen" ]; then
+    echo "Batching abd_full_pheWAS_pipeline.sh"
+    sbatch abdomen_full_pheWAS_pipeline.sh 1006
+    sbatch abdomen_full_pheWAS_pipeline.sh 1003
+    sbatch abdomen_full_pheWAS_pipeline.sh 1019
+    sbatch abdomen_full_pheWAS_pipeline.sh 17518
+    sbatch abdomen_full_pheWAS_pipeline.sh 100081
+    sbatch abdomen_full_pheWAS_pipeline.sh 1307
+    exit 0
+elif [ $1 == '-heart' ]; then
+    echo "Batching heart_full_pheWAS_pipeline.sh"
+    sbatch heart_full_pheWAS_pipeline.sh 1006
+    sbatch heart_full_pheWAS_pipeline.sh 1003
+    sbatch heart_full_pheWAS_pipeline.sh 1019
+    sbatch heart_full_pheWAS_pipeline.sh 17518
+    sbatch heart_full_pheWAS_pipeline.sh 100081
+    sbatch heart_full_pheWAS_pipeline.sh 1307
+    exit 0
+elif [ $1 == '-eye' ]; then 
+    echo "Batching eye_full_pheWAS_pipeline.sh"
+    sbatch eye_full_pheWAS_pipeline.sh 1006
+    sbatch eye_full_pheWAS_pipeline.sh 1003
+    sbatch eye_full_pheWAS_pipeline.sh 1019
+    sbatch eye_full_pheWAS_pipeline.sh 17518
+    sbatch eye_full_pheWAS_pipeline.sh 100081
+    sbatch eye_full_pheWAS_pipeline.sh 1307
+    exit 0
+fi
