a012e71e911b83addef59d4536764e75280cee31
TaykhoomDalal <tdalal@g.ucla.edu>
2022-08-16 13:16:58 -0400

    updated to match new api

--- pheWAS_pipeline/abdomen_full_pheWAS_pipeline.sh
+++ pheWAS_pipeline/abdomen_full_pheWAS_pipeline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -c 4                                                                                        # Request four cores
-#SBATCH -t 0-6:00                                                                               # Runtime in D-HH:MM format
+#SBATCH -t 0-6:30                                                                               # Runtime in D-HH:MM format
 #SBATCH -p short                                                                                    # Partition to run in
-#SBATCH --mem=64000M                                                                                # Memory total in MiB (for all cores)
+#SBATCH --mem=64GB                                                                                # Memory total in MiB (for all cores)
 #SBATCH -o /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/abdomen_pheWAS_pipeline%j.out   # File to which STDOUT will be written, including job ID (%j)
 #SBATCH -e /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/abdomen_pheWAS_pipeline%j.err   # File to which STDERR will be written, including job ID (%j)
 #SBATCH --mail-type=ALL                                                                           # Type of email notification- BEGIN,END,FAIL,ALL
@@ -10,7 +10,7 @@
 python3 ~/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/pheWAS_pipeline/full_pheWAS_pipeline.py \
                                 --phenotype /home/tad368/orig_phenos/ukb41230.tab \
                                 --bfiles ~/data_dir/bfiles \
-                                --covariates ~/data_dir/data_fields/cov_fields.txt \
+                                --covariates /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/pheWAS_pipeline/cov_fields.txt \
                                 --url https://biobank.ndph.ox.ac.uk/showcase/label.cgi?id=$1 \
                                 --samples ~/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/in_pop_IDs/Abdomen_in_sample_pop_IDs.txt \
                                 --variants /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/variants_list/Abdomen/Abdomen_Top_SNPS.tsv \
@@ -20,5 +20,7 @@ python3 ~/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/pheWAS_pipeline/full_phe
                                 --variants /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/variants_list/Abdomen/Liver_Top_SNPS.tsv \
                                 --threads 4 \
                                 --memory 64000 \
-                                --location ~/data_dir \
-                                --name ukbb_category_$1_abdomen
+                                --save_phenotypes \
+                                --location ~/data_dir/AC_included \
+                                --name ukbb_category_$1_abdomen \
+                                --pickle_intermediates
--- pheWAS_pipeline/heart_full_pheWAS_pipeline.sh
+++ pheWAS_pipeline/heart_full_pheWAS_pipeline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -c 4                                                                                        # Request four cores
-#SBATCH -t 2-00:00                                                                               # Runtime in D-HH:MM format
-#SBATCH -p medium                                                                                    # Partition to run in
-#SBATCH --mem=128000M                                                                                # Memory total in MiB (for all cores)
+#SBATCH -t 0-12:00                                                                               # Runtime in D-HH:MM format
+#SBATCH -p short                                                                                    # Partition to run in
+#SBATCH --mem=64GB                                                                                # Memory total in MiB (for all cores)
 #SBATCH -o /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/heart_pheWAS_pipeline%j.out   # File to which STDOUT will be written, including job ID (%j)
 #SBATCH -e /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/heart_pheWAS_pipeline%j.err   # File to which STDERR will be written, including job ID (%j)
 #SBATCH --mail-type=ALL                                                                           # Type of email notification- BEGIN,END,FAIL,ALL
@@ -10,15 +10,17 @@
 python3 ~/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/pheWAS_pipeline/full_pheWAS_pipeline.py \
                                 --phenotype /home/tad368/orig_phenos/ukb41230.tab \
                                 --bfiles ~/data_dir/bfiles \
-                                --covariates ~/data_dir/data_fields/cov_fields.txt \
+                                --covariates /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/pheWAS_pipeline/cov_fields.txt \
                                 --url https://biobank.ndph.ox.ac.uk/showcase/label.cgi?id=$1 \
-                                --samples ~/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/in_pop_IDs/Heart_in_sample_pop_IDs.txt \
-                                --variants /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/variants_list/Heart/Heart_Top_SNPS.tsv \
                                 --samples ~/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/in_pop_IDs/HeartMRI_in_sample_pop_IDs.txt \
                                 --variants /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/variants_list/Heart/HeartMRI_Top_SNPS.tsv \
+                                --samples ~/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/in_pop_IDs/Heart_in_sample_pop_IDs.txt \
+                                --variants /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/variants_list/Heart/Heart_Top_SNPS.tsv \
                                 --samples ~/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/in_pop_IDs/HeartECG_in_sample_pop_IDs.txt \
                                 --variants /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/variants_list/Heart/HeartECG_Top_SNPS.tsv \
                                 --threads 4 \
                                 --memory 64000 \
-                                --location ~/data_dir \
-                                --name ukbb_category_$1_heart
+                                --save_phenotypes \
+                                --location ~/data_dir/AC_included \
+                                --name ukbb_category_$1_heart \
+                                --pickle_intermediates
