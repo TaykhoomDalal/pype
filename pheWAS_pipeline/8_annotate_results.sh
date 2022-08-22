720352926f9474d5e123b0d7b20c83ab626f35e8
TaykhoomDalal <tdalal@g.ucla.edu>
2022-08-21 20:12:45 -0400

    changed dir

--- pheWAS_pipeline/8_annotate_results.sh
+++ pheWAS_pipeline/8_annotate_results.sh
@@ -7,22 +7,22 @@
 #SBATCH -e /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/batch_logs/annotate_results_%j.err   # File to which STDERR will be written, including job ID (%j)
 
 python3 /home/tad368/PheWAS-and-GWAS-Diabetes-Correlation-pipeline/pheWAS_pipeline/8_annotate_results.py \
-    --input_file /home/tad368/data_dir/AC_included/ukbb_category_1003_$1/pheWAS/aggregate_pheWAS_results.tsv \
+    --input_file /home/tad368/data_dir/AC/ukbb_category_1003_$1/pheWAS/aggregate_pheWAS_results.tsv \
     --categories 1003 \
     --singulars False \
-    --input_file /home/tad368/data_dir/AC_included/ukbb_category_1006_$1/pheWAS/aggregate_pheWAS_results.tsv \
+    --input_file /home/tad368/data_dir/AC/ukbb_category_1006_$1/pheWAS/aggregate_pheWAS_results.tsv \
     --categories 1006 \
     --singulars False \
-    --input_file /home/tad368/data_dir/AC_included/ukbb_category_1019_$1/pheWAS/aggregate_pheWAS_results.tsv \
+    --input_file /home/tad368/data_dir/AC/ukbb_category_1019_$1/pheWAS/aggregate_pheWAS_results.tsv \
     --categories 1019 \
     --singulars False \
-    --input_file /home/tad368/data_dir/AC_included/ukbb_category_1307_$1/pheWAS/aggregate_pheWAS_results.tsv \
+    --input_file /home/tad368/data_dir/AC/ukbb_category_1307_$1/pheWAS/aggregate_pheWAS_results.tsv \
     --categories 1307 \
     --singulars True \
-    --input_file /home/tad368/data_dir/AC_included/ukbb_category_17518_$1/pheWAS/aggregate_pheWAS_results.tsv \
+    --input_file /home/tad368/data_dir/AC/ukbb_category_17518_$1/pheWAS/aggregate_pheWAS_results.tsv \
     --categories 17518 \
     --singulars True \
-    --input_file /home/tad368/data_dir/AC_included/ukbb_category_100081_$1/pheWAS/aggregate_pheWAS_results.tsv \
+    --input_file /home/tad368/data_dir/AC/ukbb_category_100081_$1/pheWAS/aggregate_pheWAS_results.tsv \
     --categories 100081 \
     --singulars True \
-    --output_file /home/tad368/data_dir/AC_included/PheWAS_Visualization/ukbb_categories_aggregated_$1.tab
+    --output_file /home/tad368/data_dir/AC/PheWAS_Visualization/ukbb_categories_aggregated_$1.tab
