35c8b190b13f0cf1dac077ed3ede2706da8f903c
TaykhoomDalal <tdalal@g.ucla.edu>
2024-03-25 19:13:30 -0400

    removed old test directory

--- test_data/test_scripts/annotate_variants.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ~/pype/pype/annotate_variants.py \
-	--significant_phewas_results ~/pype/test_data/geno_phewas_1006/pheWAS/TOP_SNPS_bonferroni__aggregate_pheWAS_results.tsv \
-	--out_dir ~/pype/test_data/test_annotations
